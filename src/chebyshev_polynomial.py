49d7cd38921c1bef81ce2739fd579e0deef0e1dc
scl <solvercorleone@gmail.com>
2016-05-10 12:16:41 +0900

    finsh block version basis generation

--- src/chebyshev_polynomial.py
+++ src/chebyshev_polynomial.py
@@ -31,8 +31,32 @@ class ChebyshevPolynomial():
         return chebyshev_basis
 
 
-    def basis_generation_with_eigenvalue_shifting_and_scaling_block_vecs(self, mat, vec, step, max_eigenval, min_eigenval):
-        pass
+    def basis_generation_with_eigenvalue_shifting_and_scaling_block_vecs(self, mat, blockvec, step_val, max_eigenval, min_eigenval):
+        """ step_val >=1"""
+        assert step_val>=1, "Need a larger step_val"
+
+        block_size = blockvec.shape[1]
+        #print("block_size ", block_size)
+        chebyshev_basis = np.zeros((mat.shape[0], block_size*step_val))
+        #print("basis ", chebyshev_basis.shape[0], " , ",chebyshev_basis.shape[1])
+        op_linalg_A = linalg.aslinearoperator(mat)
+
+        s_alpha = 2.0 / (max_eigenval - min_eigenval)
+        s_beta  = - (max_eigenval + min_eigenval) / (max_eigenval - min_eigenval)
+
+        for sIdx in range(1,step_val+1):
+            degree = sIdx-1
+            if degree == 0:
+                chebyshev_basis[:,0:block_size] = blockvec[:,0:block_size]
+
+            elif degree == 1:
+                chebyshev_basis[:,block_size:2*block_size] = (s_alpha * op_linalg_A.matmat(blockvec) + s_beta * blockvec)[:,0:block_size]
+            else:
+                chebyshev_basis[:,degree*block_size:(degree+1)*block_size] = 2 * s_alpha * op_linalg_A.matmat(chebyshev_basis[:, (degree-1)*block_size:degree*block_size]) \
+                        + 2 * s_beta * chebyshev_basis[:,(degree-1)*block_size:degree*block_size] \
+                        - chebyshev_basis[:,(degree-2)*block_size:(degree-1)*block_size]
+
+        return chebyshev_basis
 
 
 
