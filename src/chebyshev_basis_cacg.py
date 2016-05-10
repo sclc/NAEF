c9e387e6e38b810aa408594b4e69ac42649d1755
scl <solvercorleone@gmail.com>
2016-05-10 12:17:00 +0900

    implement bcbcg

--- src/chebyshev_basis_cacg.py
+++ src/chebyshev_basis_cacg.py
@@ -40,7 +40,6 @@ class CBCG():
             m_AQ = op_A.matmat(m_Q)
             #m_Q_trans_AQ = linalg.aslinearoperator(m_Q.transpose())(m_AQ)
             m_Q_trans_AQ = np.matmul(m_Q.T, m_AQ)
-            #m_Q_trans_AQ_inverse = linalg.inv(m_Q_trans_AQ)
             m_Q_trans_AQ_inverse = np.linalg.inv(m_Q_trans_AQ)
             v_alpha = np.matmul( m_Q_trans_AQ_inverse, np.matmul(m_Q.T, v_r) )
 
@@ -55,6 +54,56 @@ class CBCG():
 
 
 
+class BCBCG():
+    """ """
+    def __init__(self):
+        pass
+
+    def bcbcg_solver(self, mat, RHS, init_X, step_val, tol, maxiter,whichcol):
+        gerschgorin_estimator = GerschgorinCircleTheoremEigenvalueEstimator()
+        max_eigenvalue, min_eigenvalue = gerschgorin_estimator.csr_mat_extreme_eigenvalue_estimation(mat)
+        chebyshev_basis_generator = ChebyshevPolynomial()
+
+        op_A = linalg.aslinearoperator(mat)
+
+        m_R = RHS - op_A(init_X)
+        m_X = init_X.copy()
+
+        R_to_RHS_norm_ratio = lambda x: np.linalg.norm(m_R[:,x])/np.linalg.norm(RHS[:,x])
+        residual_ratio_hist  = [R_to_RHS_norm_ratio(whichcol)]
+
+        for itercounter in range(1, maxiter+1):
+            m_chebyshev_basis = \
+                    chebyshev_basis_generator.basis_generation_with_eigenvalue_shifting_and_scaling_block_vecs(\
+                    mat, m_R, step_val, max_eigenvalue, min_eigenvalue)
+
+            #print("basis rank",np.linalg.matrix_rank(m_chebyshev_basis))
+            #return
+
+            if itercounter == 1:
+                m_Q = m_chebyshev_basis
+            else:
+                m_AQ_trans_mul_chebyshev_basis = np.matmul(m_AQ.T , m_chebyshev_basis)
+                m_B = np.matmul(m_Q_trans_AQ_inverse , m_AQ_trans_mul_chebyshev_basis)
+                m_Q = m_chebyshev_basis - np.matmul(m_Q, m_B)
+
+
+            m_AQ = op_A.matmat(m_Q)
+            m_Q_trans_AQ = np.matmul(m_Q.T, m_AQ)
+
+            m_Q_trans_AQ_inverse = np.linalg.inv(m_Q_trans_AQ)
+            m_alpha = np.matmul( m_Q_trans_AQ_inverse, np.matmul(m_Q.T, m_R) )
+
+            m_X += np.matmul(m_Q,m_alpha)
+            m_R -= np.matmul(m_AQ, m_alpha)
+
+            residual_ratio_hist.append(R_to_RHS_norm_ratio(whichcol))
+
+            print(R_to_RHS_norm_ratio(whichcol))
+            if residual_ratio_hist[-1] <= tol:
+                return m_X, m_R, residual_ratio_hist
+
+        return m_X, m_R, residual_ratio_hist
 
 
 
