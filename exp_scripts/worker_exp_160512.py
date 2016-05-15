02534e078c99efeb279f07e0814dfd57fbddbb97
scl <solvercorleone@gmail.com>
2016-05-15 21:58:20 +0900

    experiment scipt, tested matplotlib plot function

---
+++ exp_scripts/worker_exp_160512.py
@@ -0,0 +1,111 @@
+""" 
+Experiment Diary 2016-05-12
+"""
+import sys
+import math
+import matplotlib.pyplot as plt
+from scipy import io
+import numpy as np
+from scipy.sparse.linalg import *
+sys.path.append("../src/") 
+from worker import Worker
+from native_conjugate_gradient import NativeConjugateGradient
+from native_conjugate_gradient import NativeBlockConjugateGradient
+from gerschgorin_circle_theorem import GerschgorinCircleTheoremEigenvalueEstimator 
+from chebyshev_polynomial import ChebyshevPolynomial
+from chebyshev_basis_cacg import CBCG
+from chebyshev_basis_cacg import BCBCG
+
+class WorkerIterativeLinearSystemSolverCG_Exp_160507_A(Worker):
+    """ Description: Experiment A
+        Numerical Method: Naive Conjugate Gradient
+        tol:
+        max_iteration:
+        matrix:
+        Reference:
+            1. 
+    """
+    def __init__(self, mat_path):
+        """ """
+        #print ("WorkerIterativeLinearSystemSolver works good")
+        Worker.__init__(self)
+        self._hist_list = []
+
+        if mat_path == "":
+            """ Need to generatre matrix """
+            print("calling self._matrix_generation")
+            #self._mat = self._matrix_generation()
+        else:
+            self._mat_coo = io.mmread(mat_path)
+            self._mat = self._mat_coo.tocsr()
+            self._mat_info = io.mminfo(mat_path)
+            print("Done reading matrix {}, Row:{}, Col:{}".format( mat_path, self._mat.shape[0], self._mat.shape[1]))
+            print("mminfo:{}".format(self._mat_info))
+            if self._mat.getformat() == "csr":
+                print("Yeah, it is CSR")
+
+
+    def _matrix_generator(self):
+        """ generation of matrix """
+        print("_matrix_generator")
+
+
+    def _setup_testbed(self, block_size):
+        """ this can considered as a basic experiment input descripting """
+        self._B = np.random.random( ( self._mat.shape[0],block_size) )
+        np.savetxt("/home/scl/tmp/rhs.csv",self._B, delimiter=",")
+        #self._B = np.ones( ( self._mat.shape[0],6) )
+        self._X = np.ones ( (self._mat.shape[1],block_size) )
+        #self._X = np.zeros ( (self._mat.shape[1],1) ) 
+
+    def _setup_numerical_algorithm(self,tol, maxiter, step_val):
+        """ After a linear solver or other numerical methods loaded
+            we need to setup the basic prarm for the algorithm
+        """
+        self._tol = tol
+        self._maxiter = maxiter
+        self._step_val = step_val
+
+    def conduct_experiments(self, block_size, tol, maxiter, step_val):
+        """ function to condution the experiment """
+        print("to conduct the experient")
+        self._setup_testbed(block_size)
+        self._setup_numerical_algorithm(tol,maxiter,step_val)
+        #print ("before:{}".format(np.inner(self._X[:,0], self._X[:,0])))
+        self._bcbcg_exp()
+        self._try_matplotlib_simple_draw(self._residual_hist)
+        print("Experiments done")
+
+    def _bcbcg_exp(self):
+        bcbcg_solver_obj = BCBCG() 
+        self._final_X, self._final_R, self._residual_hist = \
+               bcbcg_solver_obj.bcbcg_solver(self._mat, self._B, self._X, self._step_val, self._tol, self._maxiter,0)
+
+    def _try_matplotlib_simple_draw(self, y_data):
+        Y_data = np.asarray([ math.log(y,10) for y in y_data]);
+        X_data=np.asarray( [x for x in range(0,len(y_data))] )
+        plt.plot(X_data, Y_data)
+        plt.show()
+
+
+
+def main ():
+# main function for today's experiments 
+    #mat_path = "/home/scl/MStore/nasa2146/nasa2146.mtx"
+    mat_path = "/home/scl/MStore/crystm01/crystm01.mtx"
+    #mat_path = "/home/scl/MStore/ex13/ex13.mtx"
+    #mat_path = "/home/scl/MStore/LFAT5/LFAT5.mtx"
+    block_size = 1
+    tol = 1e-10
+    maxiter = 1000
+    step_val =2 
+
+    linear_system_solver_worker_test = WorkerIterativeLinearSystemSolverCG_Exp_160507_A(mat_path)
+    linear_system_solver_worker_test.conduct_experiments(block_size,tol,maxiter, step_val)
+    #linear_system_solver_worker_test.debug_NativeConjugateGradient()
+
+
+
+if __name__ == "__main__":
+    """ call main funtion for testing """
+    main()
