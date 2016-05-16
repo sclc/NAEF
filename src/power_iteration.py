b441887293d46114cb9a780d0fbd0e474564abd7
scl <solvercorleone@gmail.com>
2016-05-16 22:19:34 +0900

    implement naive power iteration method

---
+++ src/power_iteration.py
@@ -0,0 +1,44 @@
+""" Power itertion and its variants"""
+import numpy as np
+from scipy.sparse.linalg import *
+
+class PowerIteration():
+    """ """
+    def __init__ (self):
+        pass
+
+    def naive_power_iteration (self, mat, init_x, maxiters, tol):
+        """ naive power iteration method"""
+        lambda_eigen_list=[]
+        op_spmv = aslinearoperator(mat)
+        v_eigen = init_x/ np.linalg.norm(init_x)
+        num_iter = 0
+        v_new = op_spmv(v_eigen)
+        lambda_eigen = np.inner(v_eigen[:,0], v_new[:,0])
+        lambda_eigen_list.append(lambda_eigen)
+        v_eigen = v_new / np.linalg.norm(v_new)
+
+        for idx in range(1,maxiters):
+            lambda_eigen_old = lambda_eigen
+            v_new = op_spmv(v_eigen)
+            lambda_eigen = np.inner(v_eigen[:,0], v_new[:,0])
+            lambda_eigen_list.append(lambda_eigen)
+            print("new lambda_eigen", lambda_eigen)
+            v_eigen = v_new / np.linalg.norm(v_new)
+
+            if (abs(lambda_eigen-lambda_eigen_old)/abs(lambda_eigen_old) < tol).all():
+                return v_eigen, lambda_eigen, lambda_eigen_list
+
+
+        return v_eigen, lambda_eigen, lambda_eigen_list
+
+    def power_iteration_with_shifting (self, mat, init_x, shifting_val, maxiters, tol):
+        pass
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
