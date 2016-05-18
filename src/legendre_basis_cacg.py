68caa4b15973ec7ea5b79d5b59e31ea0c88d9290
scl <solvercorleone@gmail.com>
2016-05-18 22:21:43 +0900

    legendre basis cacg scripts

---
+++ src/legendre_basis_cacg.py
@@ -0,0 +1,112 @@
+"""CBCG method """
+from gerschgorin_circle_theorem import GerschgorinCircleTheoremEigenvalueEstimator
+from legendre_polynomial import LegendrePolynomial 
+import numpy as np
+from scipy.sparse import linalg
+
+class LBCG():
+    """ """
+    def __init__(self):
+        pass
+
+    def lbcg_solver(self, mat, rhs, init_x, step_val, tol, maxiter):
+        gerschgorin_estimator = GerschgorinCircleTheoremEigenvalueEstimator()
+        max_eigenvalue, min_eigenvalue = gerschgorin_estimator.csr_mat_extreme_eigenvalue_estimation(mat)
+        #chebyshev_basis_generator = ChebyshevPolynomial()
+        legendre_basis_generator = LegendrePolynomial()
+
+        op_A = linalg.aslinearoperator(mat)
+
+        v_r = rhs - op_A(init_x)
+        v_x = init_x.copy()
+
+        s_normb = np.linalg.norm(rhs)
+        residual_ratio_hist  = [np.linalg.norm(v_r)/s_normb]
+
+        for itercounter in range(1, maxiter+1):
+            m_chebyshev_basis = \
+                    legendre_basis_generator.basis_generation_with_eigenvalue_shifting_and_scaling_single_vec(\
+                    mat, v_r, step_val, max_eigenvalue, min_eigenvalue)
+
+            if itercounter == 1:
+                m_Q = m_chebyshev_basis
+            else:
+                #op_AQ_trans = linalg.aslinearoperator(m_AQ.transpose())
+                #AQ_trans_mul_chebyshev_basis = op_AQ_trans.matmat(m_chebyshev_basis)
+                #m_B = linalg.aslinearoperator(Q_trans_AQ_inverse).matmat (AQ_trans_mul_chebyshev_basis)
+                m_AQ_trans_mul_chebyshev_basis = np.matmul(m_AQ.T , m_chebyshev_basis)
+                m_B = np.matmul(m_Q_trans_AQ_inverse , m_AQ_trans_mul_chebyshev_basis)
+                m_Q = m_chebyshev_basis - np.matmul(m_Q, m_B)
+
+            m_AQ = op_A.matmat(m_Q)
+            #m_Q_trans_AQ = linalg.aslinearoperator(m_Q.transpose())(m_AQ)
+            m_Q_trans_AQ = np.matmul(m_Q.T, m_AQ)
+            m_Q_trans_AQ_inverse = np.linalg.inv(m_Q_trans_AQ)
+            v_alpha = np.matmul( m_Q_trans_AQ_inverse, np.matmul(m_Q.T, v_r) )
+
+            v_x += np.matmul(m_Q,v_alpha)
+            v_r -= np.matmul(m_AQ, v_alpha)
+
+            residual_ratio_hist.append( np.linalg.norm(v_r)/s_normb)
+            if residual_ratio_hist[-1] <= tol:
+                return v_x, v_r, residual_ratio_hist
+
+        return v_x, v_r, residual_ratio_hist
+
+
+
+class BCBCG():
+    """ """
+    pass
+    #def __init__(self):
+    #    pass
+
+    #def bcbcg_solver(self, mat, RHS, init_X, step_val, tol, maxiter,whichcol):
+    #    gerschgorin_estimator = GerschgorinCircleTheoremEigenvalueEstimator()
+    #    max_eigenvalue, min_eigenvalue = gerschgorin_estimator.csr_mat_extreme_eigenvalue_estimation(mat)
+    #    chebyshev_basis_generator = ChebyshevPolynomial()
+
+    #    op_A = linalg.aslinearoperator(mat)
+
+    #    m_R = RHS - op_A(init_X)
+    #    m_X = init_X.copy()
+
+    #    R_to_RHS_norm_ratio = lambda x: np.linalg.norm(m_R[:,x])/np.linalg.norm(RHS[:,x])
+    #    residual_ratio_hist  = [R_to_RHS_norm_ratio(whichcol)]
+
+    #    for itercounter in range(1, maxiter+1):
+    #        m_chebyshev_basis = \
+    #                chebyshev_basis_generator.basis_generation_with_eigenvalue_shifting_and_scaling_block_vecs(\
+    #                mat, m_R, step_val, max_eigenvalue, min_eigenvalue)
+
+    #        #print("basis rank",np.linalg.matrix_rank(m_chebyshev_basis))
+    #        #return
+
+    #        if itercounter == 1:
+    #            m_Q = m_chebyshev_basis
+    #        else:
+    #            m_AQ_trans_mul_chebyshev_basis = np.matmul(m_AQ.T , m_chebyshev_basis)
+    #            m_B = np.matmul(m_Q_trans_AQ_inverse , m_AQ_trans_mul_chebyshev_basis)
+    #            m_Q = m_chebyshev_basis - np.matmul(m_Q, m_B)
+
+
+    #        m_AQ = op_A.matmat(m_Q)
+    #        m_Q_trans_AQ = np.matmul(m_Q.T, m_AQ)
+
+    #        m_Q_trans_AQ_inverse = np.linalg.inv(m_Q_trans_AQ)
+    #        m_alpha = np.matmul( m_Q_trans_AQ_inverse, np.matmul(m_Q.T, m_R) )
+
+    #        m_X += np.matmul(m_Q,m_alpha)
+    #        m_R -= np.matmul(m_AQ, m_alpha)
+
+    #        residual_ratio_hist.append(R_to_RHS_norm_ratio(whichcol))
+
+    #        print(R_to_RHS_norm_ratio(whichcol))
+    #        if residual_ratio_hist[-1] <= tol:
+    #            return m_X, m_R, residual_ratio_hist
+
+    #    return m_X, m_R, residual_ratio_hist
+
+
+
+
---
+++ src/legendre_polynomial.py
@@ -0,0 +1,67 @@
+""" """
+import numpy as np
+from scipy.sparse import linalg
+
+class LegendrePolynomial():
+    """ Chebyshev Polynomial class"""
+    def __init__(self):
+        pass
+
+    def basis_generation_with_eigenvalue_shifting_and_scaling_single_vec(self, mat, vec, step_val, max_eigenval, min_eigenval):
+        """ step_val >=1"""
+        assert step_val>=1, "Need a larger step_val"
+
+        legendre_basis = np.zeros((mat.shape[0], step_val))
+        matvec = linalg.aslinearoperator(mat)
+
+        s_alpha = 2.0 / (max_eigenval - min_eigenval)
+        s_beta  = - (max_eigenval + min_eigenval) / (max_eigenval - min_eigenval)
+
+        for sIdx in range(1,step_val+1):
+            degree = sIdx-1
+            if degree == 0:
+                legendre_basis[:,degree] = vec[:,0] 
+
+            elif degree == 1:
+                legendre_basis[:,degree] = (s_alpha * matvec(vec) + s_beta * vec)[:,0] 
+            else:
+                fl_degree = float(degree)
+                degree_reciprocal = np.reciprocal(fl_degree)
+                legendre_basis[:,degree] = degree_reciprocal * (2.*(fl_degree-1.) + 1.) * s_alpha * matvec(legendre_basis[:, degree-1]) \
+                        + degree_reciprocal * (2.*(fl_degree-1.) + 1.) * s_beta * legendre_basis[:,degree-1] \
+                        - degree_reciprocal *(fl_degree-1.) * legendre_basis[:,degree-2]
+
+        return legendre_basis
+
+
+    def basis_generation_with_eigenvalue_shifting_and_scaling_block_vecs(self, mat, blockvec, step_val, max_eigenval, min_eigenval):
+        """ step_val >=1"""
+        pass
+        #assert step_val>=1, "Need a larger step_val"
+
+        #block_size = blockvec.shape[1]
+        ##print("block_size ", block_size)
+        #chebyshev_basis = np.zeros((mat.shape[0], block_size*step_val))
+        ##print("basis ", chebyshev_basis.shape[0], " , ",chebyshev_basis.shape[1])
+        #op_linalg_A = linalg.aslinearoperator(mat)
+
+        #s_alpha = 2.0 / (max_eigenval - min_eigenval)
+        #s_beta  = - (max_eigenval + min_eigenval) / (max_eigenval - min_eigenval)
+
+        #for sIdx in range(1,step_val+1):
+        #    degree = sIdx-1
+        #    if degree == 0:
+        #        chebyshev_basis[:,0:block_size] = blockvec[:,0:block_size]
+
+        #    elif degree == 1:
+        #        chebyshev_basis[:,block_size:2*block_size] = (s_alpha * op_linalg_A.matmat(blockvec) + s_beta * blockvec)[:,0:block_size]
+        #    else:
+        #        chebyshev_basis[:,degree*block_size:(degree+1)*block_size] = 2 * s_alpha * op_linalg_A.matmat(chebyshev_basis[:, (degree-1)*block_size:degree*block_size]) \
+        #                + 2 * s_beta * chebyshev_basis[:,(degree-1)*block_size:degree*block_size] \
+        #                - chebyshev_basis[:,(degree-2)*block_size:(degree-1)*block_size]
+
+        #return chebyshev_basis
+
+
+
+
