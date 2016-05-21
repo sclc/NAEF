033e922888f34f8fbb8e2c26e262d66bd87cb123
scl <solvercorleone@gmail.com>
2016-05-21 23:29:48 +0900

    add instant_plot_unified_x_axis function

--- src/presenter.py
+++ src/presenter.py
@@ -37,6 +37,21 @@ class Presenter():
         plt.grid(True, lw=2, ls="--", c=".75")
         plt.show()
 
+    def instant_plot_unified_x_axis (self, X_data, Y_data_list, figure_title, x_label, y_label,legend_list, color_list):
+        """ plot with matplotlib"""
+        for idx in range(0,len(Y_data_list)):
+            #plt.plot(X_data, Y_data_list[idx], label=legend_list[idx] ,c=color_list[idx])
+            plt.plot(X_data, Y_data_list[idx], label=legend_list[idx] )
+
+
+        plt.title(figure_title, fontsize=20)
+        plt.xlabel(x_label, fontsize=15)
+        plt.ylabel(y_label, fontsize=30)
+        #plt.ylim(-4,4)
+        plt.legend(fancybox=True, prop={"size":16},loc="lower right")
+        plt.grid(True, lw=2, ls="--", c=".75")
+        plt.show()
+
     def instant_plot_y_log10 (self, y_data_list, figure_title, x_label, y_label,legend_list, color_list):
         """ plot with matplotlib"""
         max_value_comparator = lambda a,b: a if a>=b else b
@@ -57,7 +72,7 @@ class Presenter():
             min_x_axis = min_value_comparator(min_x_axis, min(X_data))
             min_y_axis = min_value_comparator(min_y_axis, min(Y_data))
 
-        plt.xticks(np.arange(min_x_axis, max_x_axis+1, 5.0))
+        plt.xticks(np.arange(min_x_axis, max_x_axis+1, 2.0))
         plt.yticks(np.arange(min_y_axis, max_y_axis+1, 1.))
         plt.ylim(min_y_axis-0.5, max_y_axis)
         plt.title(figure_title, fontsize=20)
