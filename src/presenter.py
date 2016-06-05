20c562b18a995d743810edca6a010df5f292139c
scl <solvercorleone@gmail.com>
2016-06-06 02:35:39 +0900

    change log10 graph font size

--- src/presenter.py
+++ src/presenter.py
@@ -65,21 +65,24 @@ class Presenter():
             assert min(y_data_list[idx]) >=.0, "you have negative value, you cannot you log10 scaling"
             Y_data = np.asarray([ math.log(y,10) for y in y_data_list[idx] ]);
             X_data = np.asarray( [x for x in range(0,len(y_data_list[idx]))] )
-            plt.plot(X_data, Y_data, label=legend_list[idx] ,c=color_list[idx])
+            #plt.plot(X_data, Y_data, label=legend_list[idx] ,c=color_list[idx])
+            plt.plot(X_data, Y_data, label=legend_list[idx] ,c=color_list[idx], linewidth=3)
 
             max_x_axis = max_value_comparator(max_x_axis, max(X_data))
             max_y_axis = max_value_comparator(max_y_axis, max(Y_data))
             min_x_axis = min_value_comparator(min_x_axis, min(X_data))
             min_y_axis = min_value_comparator(min_y_axis, min(Y_data))
 
-        plt.xticks(np.arange(min_x_axis, max_x_axis+1, 2.0))
+        plt.xticks(np.arange(min_x_axis, max_x_axis+1, 10.0))
         plt.yticks(np.arange(min_y_axis, max_y_axis+1, 1.))
+        plt.tick_params(axis="both", which="major", labelsize=35)
+        #plt.tick_params(axis="both", which="minor", labelsize=10)
         plt.ylim(min_y_axis-0.5, max_y_axis)
-        plt.title(figure_title, fontsize=20)
-        plt.xlabel(x_label, fontsize=15)
-        plt.ylabel(y_label, fontsize=30)
-        plt.legend(fancybox=True, prop={"size":16})
-        plt.grid(True, lw=2, ls="--", c=".75")
+        plt.title(figure_title, fontsize=80)
+        plt.xlabel(x_label, fontsize=60)
+        plt.ylabel(y_label, fontsize=60)
+        plt.legend(fancybox=True, prop={"size":60})
+        plt.grid(True, lw=2.5, ls="--", c=".75")
         plt.show()
 
 def main():
